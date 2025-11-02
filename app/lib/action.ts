ed84ff1a4aaeec2989f4f4ddd7609e757ae160f2
TeDi-A <tediadjene@gmail.com>
2025-11-02 19:01:50 +0100

    Insert Database

---
+++ app/lib/action.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { z } from "zod";
+import postgres from "postgres";
+import {redirect} from "next/navigation";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+const FormSchema = z.object({
+  id: z.string(),
+  customerID: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["paid", "pending"]),
+  date: z.string(),
+});
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const { customerID, amount, status } = CreateInvoice.parse({
+    customerID: formData.get("customerID"),
+    amount: parseFloat(formData.get("amount") as string), // Ensure amount is parsed as a number
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  await sql`
+  INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerID}, ${amountInCents}, ${status}, ${date});
+  `;
+
+  
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
