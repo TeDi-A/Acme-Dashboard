e1495e19fd06be6fa8278a12949a196c5895138e
TeDi-A <tediadjene@gmail.com>
2025-11-03 20:42:54 +0100

    Update CREATE

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -6,18 +6,47 @@ import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
-
 const FormSchema = z.object({
   id: z.string(),
-  customerID: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(["paid", "pending"]),
+  customerID: z.string({
+    invalid_type_error: "Please select a customer.",
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: "Please enter an amount greater than $0." }),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Please select an invoice status.",
+  }),
   date: z.string(),
 });
 
-const CreateInvoice = FormSchema.omit({ id: true, date: true });
+export type State = {
+  errors?: {
+    customerID?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
-export async function createInvoice(formData: FormData): Promise<void> {
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+export async function createInvoice(
+  prevState: State,
+  formData: FormData
+): Promise<State> {
+  const validatedFields = CreateInvoice.safeParse({
+    customerID: formData.get("customerID"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Create Invoice.",
+    };
+  }
+
   const { customerID, amount, status } = CreateInvoice.parse({
     customerID: formData.get("customerID"),
     amount: parseFloat(formData.get("amount") as string), // Ensure amount is parsed as a number
@@ -41,7 +70,6 @@ export async function createInvoice(formData: FormData): Promise<void> {
 }
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
-
 export async function updateInvoice(
   id: string,
   formData: FormData
